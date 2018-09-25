3348ff13452df7a54f8b91dd077b828e7c894ef0
RIZAL IRIANSYAH <43509313+rizaliriansyah@users.noreply.github.com>
2018-09-25 16:57:19 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,48 @@
 <?php
+if (isset($_POST['submit'])) {
+	$nama = $_POST ['nama'];
+	$nim = $_POST ['nim'];
+    $hobi1 = $_POST ['hobi1'];
+    $hobi2 = $_POST ['hobi2'];
+    $hobi3 = $_POST ['hobi3'];
+    $data.-$nama.
+}
+
+echo "<table border = 1";
+echo "<tr>"; echo "<td>"; echo "nama"; echo "br";
+echo "</td>";
+echo "<tr>"; echo "<td>"; echo "nim"; echo "br";
+echo "</td>";
+echo "<tr>"; echo "<td>"; echo "hobi1"; echo "br";
+echo "</td>";
+echo "<tr>"; echo "<td>"; echo "hobi2"; echo "br";
+echo "</td>";
+echo "<tr>"; echo "<td>"; echo "hobi3"; echo "br";
+echo "</td>";
+
+
+?>
+
+<html>
+<head>
+   <title>Form HTML</title>
+</head>
+<body>
+<form action="proses.php" method="get">
+      <p>Hobby :</p>
+      <p><input type='checkbox' name='main PUBG' value='main PUBG' />main PUBG</p>
+      <p><input type='checkbox' name='lihat youtube' value='lihat youtube' />lihat youtube</p>
+      <p><input type='checkbox' name='dengerin musik' value='dengerin musik' />dengerin musik</p>
+  
+      <form action="/action_page.php">
+      <input type="file" name="pic" accept="1243.png">
+      <input type="submit" value="input">
+      <input type='submit' name='tombol' value='kirim' />
+</form> 
+</form>
+</body>
+</html>
+
 
 
 ?>
